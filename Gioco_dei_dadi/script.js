cd3fde32a692a5b4e1aff6c0fe9f09680c62372a
christianzaboli <zabolichristian@gmail.com>
2025-09-01 17:05:09 +0200

    EX gioco dei dadi completato

---
+++ Gioco_dei_dadi/script.js
@@ -0,0 +1,26 @@
+// Gioco dei dadi
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+    
+    // GENERIAMO ALL'USER IL SUO NUMERO
+    const userNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(userNumber);
+    
+    // GENERIAMO UN NUMERO DAL COMPUTER
+    const pcNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(pcNumber);
+    
+    // STABILIAMO UN VINCITORE --- IL PIÚ ALTO
+    if (userNumber > pcNumber) {
+        console.log('Hai vinto!');
+    } else if (pcNumber < userNumber) {
+        console.log('Hai perso!');
+    } else {
+        console.log('Pareggio!');
+    }
