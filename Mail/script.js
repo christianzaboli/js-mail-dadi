f8d29bdc819cf65591759b9491b056fa34e928cd
christianzaboli <zabolichristian@gmail.com>
2025-09-01 16:33:41 +0200

    step 1 Mail - completato

--- Mail/script.js
+++ Mail/script.js
@@ -9,44 +9,15 @@ const invites = ["osazezaboli@gmail.com","christianzaboli@gmail.com","franco.ros
 console.log(invites);
 
 // INPUT USER EMAIL
-
-// CHECK CORRISPONDENZA
-// for (userEmail = prompt("Inserisci la tua email per scoprire se sei stato invitato!"); ;) {
-//     if (userEmail !== invites[i]) {
-//         console.log("Spiacente! questo utente non é nella lista degli invitati");
-//         userEmail = prompt("vuoi inserire una una email diversa?")
-//     } else {
-//         console.log("Congratulazioni! sei nella lista degli invitati");
-//         break
-//     }
-// }
-// OUTPUT ESITO
-// userEmail = prompt("Inserisci la tua email per scoprire se sei stato invitato!")
-
-// if (userEmail == invites.) {
-//     console.log("Congratulazioni! sei nella lista degli invitati");
-// } else if {
-//     console.log("Spiacente! questo utente non é nella lista degli invitati, ritenta");
-//     userEmail = prompt("Inserisci la tua email per scoprire se sei stato invitato!");
-// } else 
-
-// for (let userEmail = prompt("Inserisci la tua email per scoprire se sei stato invitato!");
-//     invites.indexOf(userEmail) >= 0;
-//     userEmail = prompt("Inserisci la tua email per scoprire se sei stato invitato!")) {
-//    if (invites.indexOf(userEmail) === -1) {
-//     console.log('b');
-//    } else {
-//     console.log("a");
-//     }
-// }
-
 let userEmail = prompt("Inserisci la tua email per scoprire se sei stato invitato!");
+
+// INIZIO CICLO FOR
 for (;;) {
     if (invites.indexOf(userEmail) !== -1) {
         console.log('Congratulazioni! sei nella lista degli invitati');
-        break;
+        break;   // CHIUSURA CICLO FOR
     } else {
         console.log("Spiacente! questo utente non é nella lista degli invitati, ritenta")
     }
-    userEmail = prompt('Riprova con una diversa')
+    userEmail = prompt('Riprova con una diversa') // CONTINUO CICLO FINO A RISPOSTA CORRETTA
 }
